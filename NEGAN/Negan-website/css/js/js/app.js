c8332116cd01362366c858eb30ca3aa99dcd3f0a
Negan110 <ordernowfromnegan@gmail.com>
2026-03-18 03:43:20 +0000

    Delete NEGAN directory

--- NEGAN/Negan-website/css/js/js/app.js
+++
@@ -1,8 +0,0 @@
-/* mobile menu */
-
-const menuBtn = document.querySelector(".menu-btn");
-const nav = document.querySelector("nav");
-
-menuBtn.addEventListener("click", () => {
-nav.classList.toggle("active");
-});
