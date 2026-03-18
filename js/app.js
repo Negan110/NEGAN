db7470e9069d49ac4b8ffb9d0420645da39c8795
Negan110 <ordernowfromnegan@gmail.com>
2026-03-18 03:50:38 +0000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/* mobile menu */
+
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
+
+menuBtn.addEventListener("click", () => {
+nav.classList.toggle("active");
+});
